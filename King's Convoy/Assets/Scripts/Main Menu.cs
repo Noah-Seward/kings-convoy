b738f5d6cbc6d2427b52a2f9424518096258835b
Trevor Matheson <Trevor.matheson@ymail.com>
2023-12-06 17:44:50 -0700

    main menu

--- King's Convoy/Assets/Scripts/Main Menu.cs
+++ King's Convoy/Assets/Scripts/Main Menu.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
+        SceneManager.LoadScene("Main Scene");
     }
 
     // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
 }
